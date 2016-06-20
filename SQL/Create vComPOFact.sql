c8a321bd0e82712743765e1045924279989b8e4b
DaveLeopold <dleopold@libertyadvisorgroup.com>
2016-06-20 16:12:24 -0500

    First draft of common PO Fact table

--- SQL/Create vComPOFact.sql
+++ SQL/Create vComPOFact.sql
@@ -1,33 +1,155 @@
 -- CREATE VIEW supplytigers.vCombined_PO_FACT AS
-    SELECT DISTINCT
-        G.
-        'Grainger' AS Vendor
-    FROM
-        supplytigers.GraingerIDR G
-    
-    UNION SELECT DISTINCT
-        K.
-        'Kaman' AS Vendor
-    FROM
-        supplytigers.KamanYTDSales K 
-    
-    UNION SELECT DISTINCT
-        O.
-        'Office Depot' AS Vendor
-    FROM
-        supplytigers.OfficeDepot O 
-        
-    UNION SELECT DISTINCT
-        ST.
-        'Staples' AS Vendor
-    FROM
-        supplytigers.StaplesOrderDetails ST 
-        
-    UNION SELECT DISTINCT
-        W.
-        'WESCO' AS Vendor
-    FROM
-        supplytigers.WESCO W
-    WHERE
-        NAME IS NOT NULL AND ADD1 IS NOT NULL
-            
+
+    SELECT 
+    G.Account_Number AS Account_Number,
+    G.Material AS MTRL_ID,
+    G.Billing_doc_Date AS Billing_Doc_Date,
+    G.Billing_document AS Billing_Document,
+    G.Cust_Prchs_Ordr_Nmbr AS Customer_Purchase_Order_Number,
+    G.Sales_document AS Sales_Document,
+    G.Contact_Name AS Contact_Name,
+    G.Sales_Office AS Sales_Office,
+    G.Sales_Office_Name AS Sales_Office_Name,
+    G.Active_Price_Point AS Active_Price_Point,
+    G.Bill_Line_No AS Bill_Line_No,
+    G.Bill_Qty AS Bill_Quantity,
+    G.Purchase_Amount AS Purchase_Amount,
+    G.Freight_Billed AS Freight_Billed,
+    G.Tax_Billed AS Tax_Billed,
+    G.Total_Invoice_Price AS Total_Invoice_Price,
+    G.Actual_Price_Paid AS Actual_Price_Paid,
+    G.WA_Price AS WA_Price,
+    G.Ext_WA_Price AS Ext_WA_Price,
+    G.Diff AS Savings,
+    (CASE
+        WHEN (G.Actual_Price_Paid < (C.AvgPrice * 0.1)) THEN 'X'
+        WHEN (G.Bill_Qty < 0) THEN 'X'
+        ELSE ''
+    END) AS IgnoreFlag,
+    'Grainger' AS Vendor
+FROM
+    (supplytigers.GraingerIDR G
+    LEFT JOIN supplytigers.vMaterialPriceAvg C ON ((G.Material = C.Material))) 
+UNION ALL SELECT 
+    K.CUST_ID AS Account_Number,
+    K.ITEM_NUM AS MTRL_ID,
+    K.BILL_DATE AS Billing_Doc_Date,
+    NULL AS Billing_Document,
+    NULL AS Customer_Purchase_Order_Number,
+    NULL AS Sales_Document,
+    K.SLSMN_NAME AS Contact_Name,
+    NULL AS Sales_Office,
+    K.branch_name AS Sales_Office_Name,
+    NULL AS Active_Price_Point,
+    NULL AS Bill_Line_No,
+    K.QTY_SHPD AS Bill_Quantity,
+    K.EXT_PRC AS Purchase_Amount,
+    NULL AS Freight_Billed,
+    NULL AS Tax_Billed,
+    NULL AS Total_Invoice_Price,
+    K.Per_Each AS Actual_Price_Paid,
+    K.Savings_Estimated_25__from_Price_Sold AS WA_Price,
+    K.Savings_Estimate_Exteded AS Ext_WA_Price,
+    K.Savings_From_Price_Purchased___Extended AS Savings,
+    (CASE
+        WHEN (K.QTY_SHPD < 1) THEN 'X'
+        ELSE ''
+    END) AS IgnoreFlag,
+    'Kaman' AS Vendor
+FROM
+    supplytigers.KamanYTDSales K 
+UNION ALL SELECT 
+    O.ACCT_Number AS Account_Number,
+    O.PRODUCT_Number AS MTRL_ID,
+    O.INV_DATE AS Billing_Doc_Date,
+    O.INV_Number AS Billing_Document,
+    O.PO_Number AS Customer_Purchase_Order_Number,
+    NULL AS Sales_Document,
+    CONCAT(O.FIRST_NAME, ' ', O.LAST_NAME) AS Contact_Name,
+    NULL AS Sales_Office,
+    NULL AS Sales_Office_Name,
+    NULL AS Active_Price_Point,
+    NULL AS Bill_Line_No,
+    O.QTY AS Bill_Quantity,
+    O.ITEM_TOTAL AS Purchase_Amount,
+    NULL AS Freight_Billed,
+    NULL AS Tax_Billed,
+    NULL AS Total_Invoice_Price,
+    O.UNIT_PRICE AS Actual_Price_Paid,
+    NULL AS WA_Price,
+    NULL AS Ext_WA_Price,
+    NULL AS Savings,
+    (CASE
+        WHEN (O.QTY < 1) THEN 'X'
+        ELSE ''
+    END) AS IgnoreFlag,
+    'Office Depot' AS Vendor
+FROM
+    supplytigers.OfficeDepot O 
+UNION ALL SELECT 
+    ST.Master_Customer_Number AS Account_Number,
+    ST.SKU AS MTRL_ID,
+    ST.OrderDate AS Billing_Doc_Date,
+    ST.Invoice_Number AS Billing_Document,
+    ST.Customer_Purchase_Order_Number AS Customer_Purchase_Order_Number,
+    ST.Order_Number AS Sales_Document,
+    ST.Order_Contact AS Contact_Name,
+    NULL AS Sales_Office,
+    NULL AS Sales_Office_Name,
+    NULL AS Active_Price_Point,
+    NULL AS Bill_Line_No,
+    ST.Qty AS Bill_Quantity,
+    ST.Adj_Gross_Sales AS Purchase_Amount,
+    NULL AS Freight_Billed,
+    NULL AS Tax_Billed,
+    NULL AS Total_Invoice_Price,
+    ST.Avg_Sell_Price AS Actual_Price_Paid,
+    ST.Unit_List_Price AS WA_Price,
+    ST.Extended_List_Price AS Ext_WA_Price,
+    ST.Discount_off_Catalog AS Savings,
+    (CASE
+        WHEN (ST.Qty < 1) THEN 'X'
+        ELSE ''
+    END) AS IgnoreFlag,
+    'Staples' AS Vendor
+FROM
+    supplytigers.StaplesOrderDetails ST
+
+/*
+UNION ALL SELECT
+    W.BR as Account_Number,
+    W.SIM as MTRL_ID,
+    W.INV_DATE as Billing_Doc_Date,
+    W.INV as Billing_Document,
+    W.Cust_PO_Number as Customer_Purchase_Order_Number,
+    W.ORDER_NO as Sales_Document,
+    null as Contact_Name,
+    null as Sales_Office,
+    null as Sales_Office_Name,
+    null as Active_Price_Point,
+    null as Bill_Line_No,
+    W.QTY as Bill_Quantity,
+as Purchase_Amount,
+    null as Freight_Billed,
+    null as Tax_Billed,
+    null as Total_Invoice_Price,
+as Actual_Price_Paid,
+as WA_Price,
+as Ext_WA_Price,
+
+    W.TSEC_per_UOM
+    W.TSEC_per_Ea  
+    W.SALES
+    W.Sales_Ext
+    W.TSEC_Ext
+
+    null as Savings,
+    (CASE
+        WHEN (W.QTY < 1) THEN 'X'
+        ELSE ''
+    END) AS IgnoreFlag,
+    'WESCO' as Vendor
+
+FROM
+    supplytigers.WESCO W
+*/
